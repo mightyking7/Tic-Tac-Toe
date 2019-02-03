815fff791fcece667de59e3d8f46f6bb49312cae
Isaac Buitrago <mightyking7@gmail.com>
2019-02-03 17:50:16 -0600

    Removed unnecessary exceptions. Changed getWinner to isWinner to check if a given letter has won.

--- game.py
+++ game.py
@@ -1,6 +1,5 @@
 
 import re
-from exceptions import TakenSquareException
 
 '''
     Responsible for controlling state of game
@@ -17,53 +16,74 @@ class Game():
         self.players = []
 
     '''
-        Starts a new match
+        Purpose:
+            Starts a new match
+            
+        Notes:
+            Starts an infinite loop that runs until a 
+            player has won or a tie has occured
     '''
     def startMatch(self):
 
         while True:
 
-            # TODO handle case when no one wins
+            # get the next player
+            self.currentPlayer = self.nextPlayer()
+            currentPlayer = self.currentPlayer
+
+            # prompt the player to make a move
+            square = self.promptPlayerMove()
+
+            # mark the square with the letter
+            self.board.markBoard(square, currentPlayer.letter)
+            currentPlayer.moves += 1
+
+            self.board.printBoard()
+
             # check if the game has been won
-            if self.getWinner():
-                print("Congratulations %s, you won !" % self.currentPlayer.name)
+            if currentPlayer.moves >= 3 and self.isWinner(currentPlayer.letter):
+                print("Congratulations %s, you won !" % currentPlayer.name)
                 break
 
-            self.currentPlayer = self.nextPlayer()
-            square = self.promptPlayerMove(self.currentPlayer.name)
-
-            try:
-                self.board.markBoard(square, self.currentPlayer.letter)
-                self.board.printBoard()
-
-            except(TakenSquareException) as e:
-                print(e.args[0])
+            # tie if X and O are only elements in the board
+            elif currentPlayer.moves >= 3 and len(set(self.board.squares)) == 2:
+                print("Tie!")
+                break
 
     '''
-        Checks the board for a winning player
+        Purpose:
+            Checks if board has been conquered by a player's letter
+        
+        Parameters:
+            l -  letter to check for a winner
+            
+        Notes:
+            Checks if any row, column, or cross on the board contain the given letter 
 
-        :return True if the game has been won, false otherwise
+        Returns:
+            True if a row, column, or cross contain l
+            False otherwise
     '''
 
-    def getWinner(self):
+    def isWinner(self, l):
+
         squares = self.board.squares
 
-        # TODO use threads to check columns, rows, and crosses indipendently
-        if (not str.isspace(squares[0]) and squares[0] == squares[1] == squares[2]) or \
-                (not str.isspace(squares[3]) and squares[3] == squares[4] == squares[5]) or \
-                (not str.isspace(squares[6]) and squares[6] == squares[7] == squares[8]) or \
-                (not str.isspace(squares[0]) and squares[0] == squares[3] == squares[6]) or \
-                (not str.isspace(squares[1]) and squares[1] == squares[4] == squares[7]) or \
-                (not str.isspace(squares[2]) and squares[2] == squares[5] == squares[8]) or \
-                (not str.isspace(squares[0]) and squares[0] == squares[4] == squares[8]) or \
-                (not str.isspace(squares[2]) and squares[2] == squares[4] == squares[6]):
-            return True
+        if squares[0] == squares[1] == squares[2]  == l or \
+            squares[3] == squares[4] == squares[5] == l or \
+            squares[6] == squares[7] == squares[8] == l or \
+            squares[0] == squares[3] == squares[6] == l or \
+            squares[1] == squares[4] == squares[7] == l or \
+            squares[2] == squares[5] == squares[8] == l or \
+            squares[0] == squares[4] == squares[8] == l or \
+            squares[2] == squares[4] == squares[6]:
+                return True
 
         return False
 
     '''
         Purpose:
-            
+            Returns the next player to make a move
     '''
     def nextPlayer(self):
 
@@ -74,23 +94,45 @@ class Game():
 
 
     '''
-        Adds a player to the game
+        Purpose:
+            Adds a player to the game
         
-        :arg player object to add to the game
+        Parameters:
+            player -  player to add
     '''
     def addPlayer(self, player):
         self.players.append(player)
 
+
     '''
-        Prompts the player to select a square to make a move  
+        Purpose:
+            Prompts the player to select which square to make a move on
+            
+        Notes:
+            The player will continue to be prompted
+            as long as they have provided invalid input.
+            
+            Valid input is a numeric value 1-9 that has
+            not been occupied on the board.
+            
+        Parameters:
+            player - prompts the player to make a move  
+            
+        Return: 
+            Number of Square  
+            
     '''
-    def promptPlayerMove(self, player):
+    def promptPlayerMove(self):
 
         square = ' '
-        while not square.isnumeric() or re.search('^[1-9]$', square) == None:
-            square = input("%s, place %c on a square (1 - 9)" %(player, self.currentPlayer.letter)).strip()
+        while not square.isnumeric() or \
+              re.search('^[1-9]$', square) == None or \
+              self.board.squares[int(square) - 1] != ' ':
 
-        return square
+            square = input("%s, place %c on a square (1 - 9)"\
+                        %(self.currentPlayer.name, self.currentPlayer.letter)).strip()
+
+        return int(square)
 
 
 class Player:
@@ -101,6 +143,7 @@ class Player:
     def __init__(self, name, letter):
         self.name = name
         self.letter = letter
+        self.moves = 0
 
 
 '''
@@ -116,7 +159,7 @@ class Board:
     ROW_COL_SIZE = 3
 
     def __init__(self):
-        self.squares = [' ' for i in range(9)]
+        self.squares = [' '] * 9
 
     '''
         Responsible for printing the contents of the board
@@ -141,16 +184,10 @@ class Board:
             square - square number on board
             
         Notes:
-            Assume that square has been validated to be 1-9
+            Assume that square is a number 1-9
     '''
     def markBoard(self, square, letter):
-
-        # check for valid square index and unmarked square
-        if self.squares[int(square) - 1] != ' ':
-            raise TakenSquareException('Square has been marked')
-
-        else:
-            self.squares[int(square) - 1] = letter
+            self.squares[square - 1] = letter
 
     '''
         Purpose:
